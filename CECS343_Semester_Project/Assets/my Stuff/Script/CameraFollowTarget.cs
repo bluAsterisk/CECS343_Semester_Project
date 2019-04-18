15eb9fe9eff534e4c08cf83cf24d52e7414d2d2a
Diaz-48 <44877038+Diaz-48@users.noreply.github.com>
2019-04-18 16:30:35 -0700

    We fucking did it

---
+++ CECS343_Semester_Project/Assets/my Stuff/Script/CameraFollowTarget.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowTarget : MonoBehaviour
+{
+    // Camera pointing at target
+    public Transform target; // What camera is following
+    private Vector3 targetCamPos; // Position of the camera following target
+
+    public float smoothing; // smooths camera following target
+    
+    // Bounds where the camera should not show
+    public float lowerBound;
+    public float leftBound;
+    public float rightBound;
+    public float upperBound;
+
+    //Leaves buffer zone where camera does not move to follow target.
+    public float bufferY, bufferX;
+
+    //public float camWidthHalf, camHeightHalf; // Leave here to check out the values of half camera size.
+    /*
+     * These are bounds for center of camera.
+     * So camera does not stop at the bounds of map
+     * but rather at the edges of the camera.
+     */
+    private float camXLB, camXRB, camYUB, camYLB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetCamPos = transform.position;
+
+        /*
+         * Used to find edges for the camera that should
+         * not show things outside of map.
+         */
+        float camWidthHalf, camHeightHalf;
+        // Assuming that Half of Camera Height is always equal to value 1; If not use Camera.main.orthographicsize;
+        camHeightHalf = Mathf.Abs(transform.position.z) * Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2);
+        // Camera aspect is width/height. Width is also half.
+        camWidthHalf = Camera.main.aspect * camHeightHalf;
+        camXLB = leftBound + camWidthHalf;
+        camXRB = rightBound - camWidthHalf;
+        camYUB = upperBound - camHeightHalf;
+        camYLB = lowerBound + camHeightHalf;
+
+        // Depending on field of view the smoothing will slow down.
+        /// Probably will remove if testing it not good
+        smoothing = smoothing / (Camera.main.fieldOfView / 10);
+    }
+
+    // Uses FixedUpdate because body is a physics object.
+    // When not using FixedUpdate the camera would leave after images as it follows target.
+    void FixedUpdate()
+    {
+        UpdateTargetCamPos();
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+    }
+    
+    // Updates Target Camera Position and make sure that the camera does not go outside of map.
+    void UpdateTargetCamPos()
+    {
+        targetCamPos.x = Mathf.Clamp(TargetXFollow(), camXLB, camXRB);
+        targetCamPos.y = Mathf.Clamp(TargetYFollow(), camYLB, camYUB);
+    }
+
+    /*
+     * If target movings outside of buffer zone then the camera will
+     * be dragged by target by adding or subtracting the buffer length.
+     */
+    float TargetYFollow()
+    {
+        // if target position is less than camera - buffer then add buffer to target position. So drag camera behind target.
+        if (target.transform.position.y < transform.position.y - bufferY)
+        {
+            return target.transform.position.y + bufferY;
+        }
+        // if target position is greater than camera + buffer then subtract buffer to target position. So drag camera behind target.
+        else if (target.transform.position.y > transform.position.y + bufferY)
+        {
+            return target.transform.position.y - bufferY;
+        }
+
+        return transform.position.y;
+    }
+
+    /*
+     * If target movings outside of buffer zone then the camera will
+     * be dragged by target by adding or subtracting the buffer length.
+     */
+    float TargetXFollow()
+    {
+        // if target position is less than camera - buffer then add buffer to target position. So drag camera behind target.
+        if (target.transform.position.x < transform.position.x - bufferX)
+        {
+            return target.transform.position.x + bufferX;
+        }
+        // if target position is greater than camera + buffer then subtract buffer to target position. So drag camera behind target.
+        else if (target.transform.position.x > transform.position.x + bufferX)
+        {
+            return target.transform.position.x - bufferX;
+        }
+
+        return transform.position.x;
+    }
+}
---
+++ CECS343_Semester_Project/Assets/my Stuff/Script/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // movement variables
+    public float maxSpeed;
+    float move;
+    
+    // jumping variables
+    bool grounded = false;
+    float groundCheckRadius = 0.05f;
+    public LayerMask groundLayer; // The layer that would trigger groundCheck if player is on ground
+    public Transform groundCheck;
+    public float jumpHeight;
+
+    // Body variables
+    Rigidbody2D pRB;
+    Animator anim;
+    bool facingRight;
+    Vector2 bodyVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initializing variables
+        pRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        facingRight = true;
+        jumpHeight *= .1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checking if jump button is pressed, if pressed jump
+        if (grounded && Input.GetButton("Jump"))
+        {
+            grounded = false;
+            anim.SetBool("isGrounded", grounded); // isGrounded is variable for Animator
+            bodyVelocityUpdate(pRB.velocity.x, jumpHeight);
+        }
+
+    }
+
+    // Called just before performing any physics calculations
+    // Physics code go to ensure they all work in sync
+    void FixedUpdate()
+    {
+
+        // Falling or grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); // check if we are grounded - if no, then we are in air
+        anim.SetBool("isGrounded", grounded); // isGrounded is variable for Animator
+        anim.SetFloat("verticalSpeed", pRB.velocity.y); // verticalSpeed is variable for Animator
+
+        // Move left or right
+        move = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(move)); // "speed" is variable for Animator
+        bodyVelocityUpdate(move * maxSpeed, pRB.velocity.y);
+
+        // Flipping character
+        if (move > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            flip();
+        }
+    }
+
+
+    /*********************************************/
+    /*********************************************/
+    // Flips character based on the direction of player movement
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector2 pScale = transform.localScale;
+        pScale.x *= -1;
+        transform.localScale = pScale;
+
+    }
+    // Updates velocity for rigidbody
+    void bodyVelocityUpdate(float x, float y)
+    {
+        bodyVelocity.Set(x, y);
+        pRB.velocity = bodyVelocity;
+    }
+}
